a34aa482580d84b2b179b174ced30160b2a4512b
https://github.com/TatianaWeb/Studio.git <tatiana.zaslavskaia@gmail.com>
2019-06-10 15:43:00 +0400

    add cats

--- js/cat.js
+++ js/cat.js
@@ -1,5 +1,23 @@
 let CatClicks = 0;
-    function catClicker() {
-        CatClicks += 1;
-        document.getElementById("CatClicks").innerHTML = CatClicks;
-    };
+function catClicker() {
+    CatClicks += 1;
+    document.getElementById("CatClicks").innerHTML = CatClicks;
+};
+
+const cats = [
+{  
+name: "Nifff",  
+imgSrc: "img/cat.jpg",
+counter: 0  
+},  
+{  
+    name: "Nufff",  
+    imgSrc: "img/cat1.jpg",  
+    counter: 0  
+},  
+{  
+    name: "Noufff",  
+    imgSrc: "img/cat2.jpg",  
+    counter: 0  
+},  
+];
